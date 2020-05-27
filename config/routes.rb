f7059e036f9fc785c8affb10d1ea7086808f5878
Pierre Valentin <pierre.valentin@wrnch.ai>
2020-05-27 01:41:36 -0400

    General application layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
-Rails.application.routes.draw do
-  resources :widgets
+# frozen_string_literal: true
 
+Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -8,5 +8,15 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
+  get 'login', to: 'users#login'
+  post 'signin', to: 'users#signin'
+  get 'logout', to: 'users#logout'
+
+  resources :users
+
+  get 'reset/password', to: 'password_recovery#index'
+  post 'reset/password', to: 'password_recovery#send_reset_email'
+  get 'set/password/:reset_password_token', to: 'password_recovery#set_password', as: 'set_password'
+  patch 'set/password/:reset_password_token', to: 'password_recovery#update_password', as: 'update_password'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
